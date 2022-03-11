93157252c72298c760e055de8d2b8770d1972b6e
Castaneche <baptiste.odet1@gmail.com>
2022-03-11 23:28:07 +0100

    homepage design changes

---
+++ index.js
@@ -0,0 +1,33 @@
+all = [
+    'fga', 'ns', 'pt', 'dice', 'minim'
+]
+main = [
+    'fga', 'ns'
+]
+univ = [
+    'pt', 'dice', 'minim'
+]
+side = []
+
+function update_projects(arr, newLink) {
+    //Update links
+    links = document.querySelectorAll("#projects .selectable");
+    for(l of links) 
+        l.classList.remove('selected');
+    document.getElementById(newLink).classList.add('selected');
+
+    //Update cards to display
+    cards = document.querySelectorAll("#projects .container .card");
+    for(c of cards) {
+        c.style.display = 'none';
+    }
+    for(a of arr) {
+        card = document.getElementById(a);
+        card.style.display = 'block';
+    }
+}
+
+document.getElementById("allprojects").addEventListener("click", function() {update_projects(all, 'allprojects')});
+document.getElementById("mainprojects").addEventListener("click", function() {update_projects(main, 'mainprojects')});
+document.getElementById("univprojects").addEventListener("click", function() {update_projects(univ, 'univprojects')});
+document.getElementById("sideprojects").addEventListener("click", function() {update_projects(side, 'sideprojects')}); 
